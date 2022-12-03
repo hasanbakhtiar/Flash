6f751009e3f2ead16c474560c8ffb578fdcc7409
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-12-03 11:45:30 +0400

    js

---
+++ JavaScript/lesson1-introduction/js/script.js
@@ -0,0 +1,20 @@
+// one line comment
+
+/**
+ * more 
+ * line 
+ * comment
+ */
+
+
+// console.log("Hello JavaScript, JavaScript is funny");
+// alert("Hello JS");
+// window.alert("js")
+
+
+// debugger;
+// console.log('hello1');
+// console.loga('hello2');
+// console.log('hello3');
+// console.log('hello4');
+// console.log('hello5');
---
+++ JavaScript/lesson2-dataTypes/js/script.js
@@ -0,0 +1,39 @@
+// Data types
+// Primitive
+// string
+// var infoStr = "JavaScript is funny.";
+// console.log(infoStr);
+
+// number
+// var infoNumbOne = "5";
+// var infoNumbTwo = 10;
+// console.log(infoNumbOne + infoNumbTwo);
+
+// boolean
+// true
+// false
+
+// if (false) {
+//     console.log('JavaScript is best');
+// }else{
+//     console.log('JavaScript is good');
+// }
+
+
+
+// Composite
+// array
+// var infoArr = ['hello',1,true];
+// console.log(infoArr);
+// object
+// var infoData = {
+//     title:"JavaSctipt",
+//     level:'hight'
+// }
+// console.log(infoData.title);
+
+// Special
+// undefined
+// var infoUn = null;
+// console.log(infoUn);
+// null
