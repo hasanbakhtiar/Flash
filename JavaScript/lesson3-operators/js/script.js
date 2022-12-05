f55c61f2ef5588c67558678bc5503bc0570b4789
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-12-05 16:50:46 +0400

    operators

---
+++ JavaScript/lesson3-operators/js/script.js
@@ -0,0 +1,93 @@
+// assignment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+// arithmetic operators 
+// +
+// -
+// *
+// /
+// %
+// **
+// ++
+// --
+
+// comparison operators
+// ==
+// ===
+// !=
+// !==
+// < 
+// <= 
+// > 
+// >= 
+
+// logical operators
+// && - AND
+// || - OR
+// !  - NOT
+
+// string operators
+// +
+// +=
+
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+
+
+// \            (tərs slash)(Qaçındırma operatorudur)
+
+
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+
+// ?:           (sual üst-üstə qoşa nöqtə)((şərt) ? ifadə1 : ifadə2)(ternary)
+// şərt            ? true                 : false
+
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+
+// delete       (objectin dəyərini silmək üçündür)
+
+
+// new          (yeni dəyər yaratməq üçündür)
+
+class Car {
+    constructor(brand,model,year){
+        this.ibrand = brand;
+        this.imodel = model;
+        this.iyear = year;
+    }
+}
+
+var infoCar = new Car("BMW","X5",2022);
+console.log(infoCar.ibrand);
+
+console.log(infoCar instanceof Car);
+
+// this         (mövcud dəyəri işarələmək üçündür)
+
+// in           (dəyişkənin(object) daxilində dəyərin olub olmamasını yoxlamaq üçündür)
+
+
+
+
+
+// instanceof   (dəyişkənin(objectin xüsusiyyətinin) içində xüsusiyyət olub olmamasını yoxlamaq üçündür)
+
+
+
+  
+
+
+
+    
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
---
+++ JavaScript/lesson4-variableCondition/js/script.js
@@ -0,0 +1,61 @@
+// 1.Dəyişkən adları mənalı və anlaşıqlı olmalıdır.
+// 2.Dəyişkən adlarını yazarkən İngilis əlifbasından başqa əlifadan istifadə etmək qadağandır.
+// 3.Dəyişkən adları _ (alt xətt) ilə başlaya bilər.
+// 4.Dəyişkən adları $(dollar) işarəsi ilə başlaya bilər.
+// 5.Dəyişkən adları heç bir zaman rəqəm ilə başlaya bilməz.Amma ilk hərfdə sonra istifadə edə bilərik.
+// 6.Dəyişkən adları böyük kiçik hərfləri anlayır.
+// 7.Eyni dəyişkənə iki fərqli məlumat tanımlanan zaman sonuncu tanımlanan məlumatı göstərir.
+// 8.JavaScript tərəfindən hazırlanmış rezerv kəlimələri dəyişkən adlarına vermək qadağandır.
+// 9.Eyni dəyişkən adını dəişkən ifadələrində istifadə etmək qadağandır.
+
+// abstract 	    arguments	    await*	            boolean
+// break	        byte	        case	            catch
+// char	            class*	        const	            continue
+// debugger	        default	        delete	            do
+// double	        else	        enum*	            eval
+// export*	        extends*	    false	            final
+// finally	        float	        for	                function
+// goto	            if	            implements	        import*
+// in	            instanceof	    int	                interface
+// let*	            long	        native	            new
+// null	            package	        private	            protected
+// public	        return	        short	            static
+// super*	        switch	        synchronized	    this
+// throw	        throws	        transient	        true
+// try	            typeof	        var	                void
+// volatile	        while	        with	            yield
+
+// var        -> variable (global)
+// let        -> let      (local)
+// const      -> constant (local)
+
+// global
+
+// if (true) {
+//     // local global
+//     if (true) {
+//         // local global
+//         if (true) {
+//             let a = 5;
+//             // local
+//         }
+//     }
+//     console.log(a);
+// }
+
+// var a  =5;
+// a  =10;
+// console.log(a);
+
+// if  - eger
+// else - deyilse
+// else if  - deyilse eger
+
+var info =10;
+if (info == 5) {
+    console.log('this is 5');
+}else if(info == 10){
+        console.log('this is 10');
+}else{
+    console.log('something is wrong');
+}
