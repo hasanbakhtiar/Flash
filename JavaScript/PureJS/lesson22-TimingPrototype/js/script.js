75061cdd7e3186f9255f7c21ccd6a21cd4f3f4d8
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-03-31 17:56:04 +0400

    js

---
+++ JavaScript/PureJS/lesson22-TimingPrototype/js/script.js
@@ -0,0 +1,43 @@
+
+// function Car(brand,model,year){
+//     this.ibrand = brand;
+//     this.imodel = model;
+//     this.iyear = year;
+// }
+
+// Car.prototype.color = "Blue";
+
+
+// const myCar = new Car("BMW","X5",2022);
+// console.log(myCar);
+
+// const myData = new Car("Hello",'Mello',1);
+
+
+// console.log(myCar.color);
+
+
+// class Car{
+//     constructor(brand,model,year){
+//         this.ibrand = brand;
+//         this.imodel = model;
+//         this.iyear = year;
+//     }
+//     calculateSpeed(km,hour){
+//         return `Speed->${this.ibrand}: ${(km/hour).toFixed()}km/h`
+//     }
+// }
+
+
+// class Moto extends Car{
+//     constructor(brand,model,year){
+//       super(brand,model,year);
+//     }
+  
+// }
+
+
+// const myCar = new Car("BMW","X5",2022);
+// const myMoto = new Moto("Yamaha","S500",2002);
+
+// console.log(myMoto.calculateSpeed(500,4));
