f9a507ee07655a3299a52ba6de5956fdd7580d61
hasanbakhtiar <hasan.doctype@gmail.com>
2023-04-17 20:45:00 +0400

    hoo

---
+++ JavaScript/ReactJS/lesson8-hookenter/src/App.jsx
@@ -0,0 +1,33 @@
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Class Component</h1>
+//       </div>
+//     )
+//   }
+// }
+
+import List from "./List"
+
+
+// function App (){
+//   return(
+//     <h1>Function Component</h1>
+//   )
+// }
+
+
+const App =()=>{
+  return(
+    <>
+    <List title="HelloData" year="2000"/>
+    </>
+  )
+}
+
+
+
+export default App
---
+++ JavaScript/ReactJS/lesson8-hookenter/src/List.jsx
@@ -0,0 +1,14 @@
+
+
+const List = ({title,year}) => {
+  return (
+    <div>
+        <ol>
+            <li>{title}</li>
+            <li>{year}</li>
+        </ol>
+    </div>
+  )
+}
+
+export default List
